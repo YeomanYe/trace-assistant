2451012414bf6f3dc048f5aad418e83251d0025c
YeomanYe <ming13821007683@gmail.com>
2018-01-30 23:29:47 +0800

    add lib

--- js/content.js
+++ js/content.js
@@ -1,8 +1,15 @@
 /*var img = new Image();
-img.src = chrome.runtime.getURL("images/comic.png");
-img.setAttribute('style','position:absolute;bottom:20px;right:20px;z-index:999');
+img.setAttribute('style','position:fixed;bottom:20px;width:80px;right:20px;z-index:999');
 document.body.appendChild(img);*/
-var div = document.createElement('div');
-div.className = 'comic';
-div.setAttribute('style','position:absolute;bottom:20px;right:20px;z-index:999;width:30px;height:30px');
-document.body.appendChild(div);
+$(function(){
+    var $img = $('<img>');
+    $img.get(0).src = chrome.runtime.getURL('images/comic.png');
+    $img.css({
+        position:'fixed',
+        bottom:'20px',
+        right:'20px',
+        width:'80px',
+        'z-index':999
+    });
+    $('body').append($img);
+})
