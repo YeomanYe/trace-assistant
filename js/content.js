e27c86a6741cd1f2858c1c0a0c7c39605ef19402
YeomanYe <ming13821007683@gmail.com>
2018-01-31 23:48:28 +0800

    add syncStor

--- js/content.js
+++ js/content.js
@@ -12,4 +12,45 @@ $(function(){
         'z-index':999
     });
     $('body').append($img);
-})
+    $img.on('click',fetchUserCol);
+});
+var storSync = chrome.storage.sync;
+function fetchUserCol(){
+    var href = location.href;
+    var baseUrl = 'http://www.kuaikanmanhua.com/web/fav/topics',
+        pageNum = 1,
+        size = 16;
+    if(href.search('kuaikan')<0)return;
+    var cols = []; //表示收藏的集合
+    var successCallback = function(resData){
+        console.log('resData',resData);
+        var datas = resData.data.topics;
+        var storDatas = [];
+        datas.forEach(function(data){
+            storDatas.push({
+                imgUrl:data.cover_image_url,
+                url:'http://www.kuaikanmanhua.com/web/topic/'+data.id,
+                title:data.title
+            });
+        });
+        cols = cols.concat(storDatas);
+        if(datas.length == size) {
+          ++pageNum;
+          var url = baseUrl+'?page='+pageNum+'&size='+size;
+          console.log('url',url);
+          $.ajax(url,{
+            success:successCallback
+          });
+        }else{
+          storSync.set({
+            'kukuCol':cols
+          });
+          storSync.get('kukuCol',function(array){
+            console.log(array);
+          });
+        }
+    };
+    $.ajax(baseUrl+'?page='+pageNum+'&size='+size,{
+        success:successCallback
+    });
+}
