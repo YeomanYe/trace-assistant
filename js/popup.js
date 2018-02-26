7c938427b78d2b862039a76112c1e155f29581f7
YeomanYe <ming13821007683@gmail.com>
2018-02-26 22:56:58 +0800

    update UI

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,22 @@
 var storSync = chrome.storage.sync;
-storSync.get('kukuCol',function(array){
-    console.log(array);
+var log = console.log;
+storSync.get('kukuCol',function(resObj){
+    log(resObj);
+    resObj = resObj.kukuCol;
+    var baseImg = resObj.baseImg,
+        baseIndex = resObj.baseIndex,
+        cols = resObj.cols;
+    log(baseImg,baseIndex);
+    cols.forEach(function(obj){
+        liTempStr = $('#listItemTemplate').html();
+        $liInstance = $(liTempStr);
+        $liInstance.find('img')[0].src = baseImg + obj.imgUrl;
+        $liInstance.find('.right h3').text(obj.title);
+        $liInstance.find('.right .news').text(obj.latestChapter);
+        // $liInstance.find('.right .current').text(obj.latestChapter);
+        log($liInstance);
+        $('.list').append($liInstance);
+    });
+        
+
 });
