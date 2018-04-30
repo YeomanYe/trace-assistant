84fc733fc6e3bfa65d44ad320e9444be36acfca7
YeomanYe <ming13821007683@gmail.com>
2018-04-30 13:55:43 +0800

    delete getBaseUrl

--- js/utils.js
+++ js/utils.js
@@ -13,19 +13,6 @@ var _createQueryObj = Object.create(_createQueryObjProto);
 var _exportFunObj = {};
 
 /**
- * 获取两个URL中相同的部分
- */
-function getBaseUrl(url1, url2) {
-    var arr1 = url1.split('/'),
-        arr2 = url2.split('/');
-    var sameArr = [];
-    for (var i = 0, len = arr1.length; i < len; i++) {
-        if (arr1[i] === arr2[i]) sameArr.push(arr1[i]);
-    }
-    return sameArr.join('/');
-}
-
-/**
  * 判断数组中是否有一个字符串在目标字符串中。并返回序列号。-1代表不存在
  * field代表数组中某个对象的某一字段
  */
