ae86fd8907350922a0b8b19b38e4fb731f0978d9
YeomanYe <ming13821007683@gmail.com>
2018-06-07 23:47:43 +0800

    add checkbox

--- js/component/fav-list.js
+++ js/component/fav-list.js
@@ -5,6 +5,18 @@ var vFavItem = Vue.component('fav-list', {
   template: `
 <ul id="favList" class="list">
     <li v-for="(item,index) in items"   :key="item.indexUrl">
+    <!-- checkbox -->
+    <div class="wrapCheckbox pretty p-svg p-curve">
+            <input type="checkbox" />
+            <div class="state p-success">
+                <!-- svg path -->
+                <svg class="svg svg-icon" viewBox="0 0 20 20">
+                    <path d="M7.629,14.566c0.125,0.125,0.291,0.188,0.456,0.188c0.164,0,0.329-0.062,0.456-0.188l8.219-8.221c0.252-0.252,0.252-0.659,0-0.911c-0.252-0.252-0.659-0.252-0.911,0l-7.764,7.763L4.152,9.267c-0.252-0.251-0.66-0.251-0.911,0c-0.252,0.252-0.252,0.66,0,0.911L7.629,14.566z" style="stroke: white;fill:white;"></path>
+                </svg>
+                <label ></label>
+            </div>
+        </div>
+        <!-- img -->
         <a :href="item.indexUrl" target="_blank" class="left">
             <img :src="item.imgUrl" @error="imgLoseHandler"/>
         </a>
--- js/component/toolbar.js
+++ js/component/toolbar.js
@@ -1,25 +1,31 @@
 var vToolbar = Vue.component('toolbar', {
-  props: {
-  },
-  template: `
+    props: {},
+    data: function () {
+        return {
+            bdArr: [
+                {
+                    title: '给个好评',
+                    img: '../../images/praise.png',
+                    href: 'https://chrome.google.com/webstore/detail/%E8%BF%BD%E7%BB%BC%E9%A5%AD/fajeglfbhflmbaccedmbgelodcbljobl/reviews?utm_source=chrome-ntp-icon'
+                },
+                {title: '批量操作', img: '../../images/batch.png'}
+            ]
+        }
+    },
+    template: `
           <header id='toolbar'>
             <div class="hd">
                 <img src="../../images/icon/logo-white32.png" alt=""/>
                 <h1>追综饭</h1>
             </div>
-            <div class="bd">
-                <a target="_blank" title="给个好评" href="https://chrome.google.com/webstore/detail/%E8%BF%BD%E7%BB%BC%E9%A5%AD/fajeglfbhflmbaccedmbgelodcbljobl/reviews?utm_source=chrome-ntp-icon">
-                    <img src="../../images/praise.png" alt=""/>
-                </a>
-            </div>
-            <div class="bd">
-                <a target="_blank" title="批量操作" href="javascript:;">
-                    <img src="../../images/batch.png" alt=""/>
-                </a>
-            </div>
+            <template v-for="(bdObj,index) in bdArr" >
+                <div class="bd">
+                    <a target="_blank" :title="bdObj.title" :href="bdObj.href || 'javascript:;'">
+                        <img :src="bdObj.img" alt=""/>
+                    </a>
+                </div>
+            </template>
         </header>
      `,
-  methods: {
-
-  }
+    methods: {}
 })
